3dbf9fb67e909ab1a1efa8203e83adf4e2d78fc2
Brenden Krueger <bkruegercomputerscience@gmail.com>
2022-01-12 08:25:47 -0800

    Initial Sandbox Commit

---
+++ Sandbox.js
@@ -0,0 +1,12 @@
+/*
+Programmer: Brenden Krueger
+Date: 1-12-2022
+Program: Sandbox
+*/
+
+
+async function startProgram() {
+	speak("Hello World!");
+	setMainLed({r: 255, g: 0, b: 0});
+	roll(0,50,2);
+}
