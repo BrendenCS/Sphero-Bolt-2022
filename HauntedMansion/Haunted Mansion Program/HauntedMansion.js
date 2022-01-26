4d087eb18261d7b19da892a5aff34b858f58d3a0
Brenden Krueger <bkruegercomputerscience@gmail.com>
2022-01-26 09:36:23 -0800

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/Haunted Mansion Program/HauntedMansion.js
@@ -0,0 +1,7 @@
+/*
+Programer: Mr. Lange
+Team Member: Dominic
+Date: 1-26-2022
+Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted
+Sounds, Changing of Colors, and other obstacles along the way using a Shero Bolt and Java Script.
+*/
