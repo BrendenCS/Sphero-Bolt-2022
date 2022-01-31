d21b2939cd8513fb26f14a1e01292232f7a2a600
Brenden Krueger <bkruegercomputerscience@gmail.com>
2022-01-31 09:13:18 -0800

    Initial Haunted Mansion Commit - Music Note #1 - Brenden

--- HauntedMansion/Haunted Mansion Program/HauntedMansion.js
+++ HauntedMansion/Haunted Mansion Program/HauntedMansion.js
@@ -5,3 +5,18 @@ Date: 1-26-2022
 Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted
 Sounds, Changing of Colors, and other obstacles along the way using a Shero Bolt and Java Script.
 */
+
+async function startProgram() {
+	// Write code here
+	
+	await scrollMatrixText('Start', { r: 255, g: 245, b: 0 }, 15, false);
+	await roll(0, 50, 3);
+	setMainLed({ r:0, g:0, b: 255 });
+	stopRoll();
+	setHeading(90);
+	await roll(0, 40, 1);
+	await roll(90,40,4)
+	await speak('music note number 1');
+
+ 
+}
